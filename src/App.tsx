8f8599dfc5e0685e6acbec4ca045f1873461f0b5
eyobScript <eyobbmulugeta@gmail.com>
2025-06-27 02:09:38 +0300

    s

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from 'react'
-import './App.css'
 import { Button , HStack} from '@chakra-ui/react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div >

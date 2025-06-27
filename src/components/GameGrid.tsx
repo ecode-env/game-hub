78ee39b411af162dfcac773b035e7bb56d37c9e3
eyobScript <eyobbmulugeta@gmail.com>
2025-06-28 00:38:00 +0300

    Fetch the games

---
+++ src/components/GameGrid.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import apiClint from "../service/api-clint";
+import { Text } from "@chakra-ui/react";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const GameGrid = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    apiClint
+      .get<FetchGamesResponse>("/games")
+      .then((res) => {
+        setGames(res.data.results);
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  });
+  return (
+    <>
+      {error && <Text color="red">{error}</Text>}
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default GameGrid;
---
+++ src/service/api-clint.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: "https://api.rawg.io/api", 
+    params: {
+        key: "25675629453840a996d4e50f9f7463de",
+    },
+});

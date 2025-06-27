fb843cc856b8d2faf1d9f4fbc795844c558cec4f
eyobScript <eyobbmulugeta@gmail.com>
2025-06-28 02:39:07 +0300

    Get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Game } from "../hooks/useGames";
 import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../service/image-url";
 
 interface Prop {
   game: Game;
@@ -10,7 +11,7 @@ interface Prop {
 const GameCard = ({ game }: Prop) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
---
+++ src/service/image-url.ts
@@ -0,0 +1,21 @@
+
+
+const getCroppedImageUrl = (url: string) => {
+  const target = 'media/';
+  const index = url.indexOf(target) + target.length;
+  return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
+}
+
+
+// or 
+
+// const getCroppedImageUrl = (url: string): string => {
+//   // Split the URL at 'media/'
+//   const [before, after] = url.split('media/');
+  
+//   // Rebuild the URL with 'crop/600/400/' inserted
+//   return `${before}media/crop/600/400/${after}`;
+// };
+
+
+export default getCroppedImageUrl;

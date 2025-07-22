ccc0dec5499e0a05f41a6ac82e541a0871652872
eyobScript <eyobbmulugeta@gmail.com>
2025-07-23 00:54:44 +0300

    Refactoring- create a reusable API client

--- src/service/api-clint.ts
+++ src/service/api-clint.ts
@@ -1,16 +1,29 @@
-import axios, { CanceledError } from "axios";
+import axios, { AxiosRequestConfig, CanceledError } from "axios";
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-
-
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "25675629453840a996d4e50f9f7463de",
   },
 });
-export { CanceledError };
+
+class APIClint<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config?: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClint;

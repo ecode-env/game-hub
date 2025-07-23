ab5fdb03d7a6f5b56ac62518140c5bb093f58329
eyobScript <eyobbmulugeta@gmail.com>
2025-07-23 21:32:46 +0300

    refactor- simplify game queries

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,14 +1,19 @@
 import { Heading } from "@chakra-ui/react";
 import { GameQuery } from "../App";
+import useGenres from "../hooks/useGenres";
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameHeading = ({ gameQuery }: Props) => {
-  const heading = `${gameQuery.platform?.name || ""} ${
-    gameQuery.genre?.name || ""
-  } Game`;
+  const { data: genres } = useGenres();
+  const { data: platforms } = useGenres();
+
+  const genre = genres.results.find((g) => g.id === gameQuery.genreId);
+  const platform = platforms.results.find((p) => p.id === gameQuery.platformId);
+
+  const heading = `${platform?.name || ""} ${genre?.name || ""} Game`;
   return (
     <Heading as="h1" marginY={5} fontSize="5xl">
       {heading}
